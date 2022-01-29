aef3e437d8b18188ea8db36b1a42cae4cedb36cc
Karpfly <karpfly@gmail.com>
2022-01-29 17:32:43 +0100

    cleanup

--- js/conversion.js
+++ js/conversion.js
@@ -34,7 +34,6 @@ $(document).ready(function () {
                         + '<p>Note: This could take a considerable amount of time depending on your hardware and the preset you chose. You can safely close this window.</p>'
                         + '</div>'
                         + '<div id="params">'
-                        + '</p>'
                         + '<br>'
                         + '<div class="checkbox-container">'
                         + '</div></div>'
